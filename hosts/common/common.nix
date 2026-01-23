3c2b1c592bfda3b49e8b7f218eec48d64952ee47
ajhyperbit <ajhyperbit@gmail.com>
2026-01-23 16:14:19 -0600

    removed a space

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -325,7 +325,7 @@ in
       settings = {
         default_session = {
           user = username;
-          command = "${pkgs.tuigreet}/bin/tuigreet --time -w 120 --remember-session "; # start Hyprland with a TUI login manager
+          command = "${pkgs.tuigreet}/bin/tuigreet --time -w 120 --remember-session"; # start Hyprland with a TUI login manager
         };
       };
     };
