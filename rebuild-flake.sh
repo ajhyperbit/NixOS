e0e073ff0ef571bdce313079cd849b27cd6eb5d4
ajhyperbit <ajhyperbit@gmail.com>
2026-01-23 15:38:24 -0600

    had a broken rebuild-flake script apparently

--- rebuild-flake.sh
+++ rebuild-flake.sh
@@ -7,7 +7,7 @@ usage () {
     printf "rebuild method:\t Rebuild methods are either switch, boot, test, build, or dry-activate.\n"
     printf "Arguments put after the ones listed above will be used as arguments for nixos-rebuild command.\n\n"
     printf "More details on rebuild methods here: https://nixos.wiki/wiki/Nixos-rebuild\n"
-
+}
 
 if [ $# -eq 1 ]; then      # if help requested
     if [ $1 = "-h" ]; then
@@ -21,16 +21,16 @@ if [ $# -eq 1 ]; then      # if help requested
     printf "Don't recognize your option exiting...\n\n"
     usage
     exit 2;
+fi
 
-
-
-
+host=${1:-}
+reswitch=${2:-}
 # Capture all arguments into a variable for use with nixos-rebuild
 args=${@:3}  # Capture arguments starting from the 3rd argument
 user=$LOGNAME 
 #if [ -z "$user" ] then
-
-
+#user=$(logname)
+#fi
 #LINK - https://unix.stackexchange.com/questions/479102/how-can-i-filter-read-only-file-systems-out-of-df-output#:~:text=df%20%2D%2Doutput%3Dpcent%2Ctarget%20%24(mount%20%2Dt%20ext4%20%7C%20grep%20rw%20%7C%20cut%20%2Dd%22%20%22%20%2Df1)
 storage=$(df --output=pcent,target $(mount -t ext4 | grep rw | cut -d" " -f1) | head -n -1)
 
@@ -43,7 +43,7 @@ if [ -z "$host" ] || [ -z "$reswitch" ]; then
     printf "  <rebuild method>: 'switch', 'boot', 'test', 'build', or 'dry-activate'\n"
     printf "For more help, use -h or --help\n"
     exit 3;
-
+fi
 
 #Code block for choices
 choose () {
@@ -72,7 +72,7 @@ choose () {
             exit 3;
             ;;
     esac
-
+}
 
 choose "n" "Do you want to update flake.lock? [(Y)es/(N)o] (Default: No): " "source ~/NixOS-Hyprland/update-flake.sh"
 
@@ -84,17 +84,17 @@ set -o pipefail
 if command -v nom >/dev/null 2>&1 && command -v unbuffer >/dev/null 2>&1; then
     sudo -v
     sudo unbuffer nixos-rebuild "$reswitch" --upgrade --show-trace --flake .#"$host" --log-format internal-json |& tee nixos-switch.log | nom --json 
-
+else
     sudo nixos-rebuild "$reswitch" --upgrade --show-trace --flake .#"$host" 2>&1 | tee nixos-switch.log
-
+fi
 
 #REVIEW - Testing required
 #NOTE - If the above command doesn't function correctly, then the if statement below can replace it.
 #if [ -z "args" ]; then
 #sudo nixos-rebuild "$reswitch" --upgrade --show-trace --flake .#"$host" &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1) || grep -P -n "(?|(\/home\/"$user"\/NixOS-Hyprland\/([a-zA-Z]+)\.nix)|(hosts\/([a-zA-Z]+)\/([a-zA-Z]+).nix))" nixos-switch.log | sed 's/:[[:blank:]]*/: /'
-
+#else
 #sudo nixos-rebuild "$reswitch" --upgrade --show-trace --flake .#"$host" "$args" &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1) || grep -P -n "(?|(\/home\/"$user"\/NixOS-Hyprland\/([a-zA-Z]+)\.nix)|(hosts\/([a-zA-Z]+)\/([a-zA-Z]+).nix))" nixos-switch.log | sed 's/:[[:blank:]]*/: /'
-
+#fi
 #REVIEW - Testing required
 
 current_tag=$(nixos-rebuild list-generations | grep True | grep -Eo '[0-9]+' | head -1)
@@ -109,7 +109,7 @@ hash=$(git rev-parse --short HEAD)
 
 if [[ $(git status --short) != '' ]]; then
   dirty='-dirty'
-
+fi
 
 if [ $last_tag != "Gen-$hostname-$current_tag-$hash$dirty" ]; then
     git tag Gen-$hostname-$current_tag-$hash$dirty
@@ -117,14 +117,14 @@ if [ $last_tag != "Gen-$hostname-$current_tag-$hash$dirty" ]; then
     printf "Last tag: "$last_tag"\n"
 
     choose "y" "Do you want to push the tag Gen-"${hostname}"-"${current_tag}"-"${hash}${dirty}"? [(Y)es/(N)o/(Q)uit] (Default: Yes): " "git push origin tag Gen-$host-$current_tag-$hash$dirty"
-
+fi
 
 #REVIEW - Testing required
 #if ["$hostname" == "nixos"]; then
 #printf "\n"%s"\n" "$storage"
-
-
-
+#else
+#:
+#fi
 #REVIEW - Testing required
 
 #TODO: add a way to run nix-collect garbage with sudo?
