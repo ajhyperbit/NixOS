7057e413d2f051169dc727750cc5f1b523b1b68c
ajhyperbit <ajhyperbit@gmail.com>
2026-02-04 18:40:33 -0600

    add handling to update-flake script for SSH commits

--- update-flake.sh
+++ update-flake.sh
@@ -2,5 +2,9 @@
 nix flake update
 #Add flake.lock to staged changes
 git add flake.lock
+#Handle if we're connected via SSH
+if [[ -n "$SSH_TTY" ]]; then
+export GPG_TTY=$(tty)
+fi
 #Commit the changes so that there isn't a "git tree dirty" warning
 git commit -m "chore: update flake.lock"
