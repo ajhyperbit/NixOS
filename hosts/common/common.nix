ff9c0cafb26362910bad8538fdc2e7be90460d97
ajhyperbit <ajhyperbit@gmail.com>
2026-02-11 14:38:51 -0600

    add llsblk alias

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -186,6 +186,7 @@ in
       fl = "findlink";
       rebuild = "/home/${username}/NixOS-Hyprland/rebuild-flake.sh";
       clean = "nh clean all ${nhArgs}";
+      llsblk = "lsblk -o NAME,KNAME,SIZE,TYPE,MOUNTPOINT,SERIAL";
     };
   };
 
