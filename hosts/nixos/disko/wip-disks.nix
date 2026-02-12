192222c77290036fb839d486fe274d8c1a29028d
ajhyperbit <ajhyperbit@gmail.com>
2026-02-12 12:47:00 -0600

    update DATA.nix, works correctly now rename DATA.nix to wip-disks.nix, since trying to add more than just the one disk now

--- hosts/nixos/disko/wip-disks.nix
+++ hosts/nixos/disko/wip-disks.nix
@@ -2,6 +2,7 @@
 
 let
   disk4 = "/dev/disk/by-id/ata-ST2000NE0025-2FL101_ZDS1968N";
+  disk5 = "/dev/disks/by-id/ata-ST6000VN0033-2EE110_ZADBCVNZ";
 in
 {
   disko.devices = {
@@ -15,23 +16,23 @@ in
             DATA = {
               label = "DATA";
               size = "100%";
-
               content = {
                 type = "btrfs";
                 extraArgs = [ "-f" ];
-
                 subvolumes = {
-                  "/DATA" = {
+                  "/mnt/DATA" = { };
+                  "/DATA/rootfs" = {
                     mountpoint = "/DATA";
                     mountOptions = [
                       "compress=zstd"
+                      "nofail"
                     ];
                   };
-
                   "/DATA/.snapshots" = {
                     mountpoint = "/DATA/.snapshots";
                     mountOptions = [
                       "compress=zstd"
+                      "nofail"
                     ];
                   };
                 };
