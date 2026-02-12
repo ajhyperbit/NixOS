55028eb09bcd9f9d2536abdf12524a43b7020e30
ajhyperbit <ajhyperbit@gmail.com>
2026-02-12 13:16:12 -0600

    I think the DATA drive stuff is correct this time? Removed Archive drive stuff, still has data on it

--- hosts/nixos/disko/wip-disks.nix
+++ hosts/nixos/disko/wip-disks.nix
@@ -2,7 +2,6 @@
 
 let
   disk4 = "/dev/disk/by-id/ata-ST2000NE0025-2FL101_ZDS1968N";
-  disk5 = "/dev/disks/by-id/ata-ST6000VN0033-2EE110_ZADBCVNZ";
 in
 {
   disko.devices = {
@@ -20,8 +19,8 @@ in
                 type = "btrfs";
                 extraArgs = [ "-f" ];
                 subvolumes = {
-                  "/mnt/DATA" = { };
-                  "/DATA/rootfs" = {
+                  "/DATA" = { };
+                  "/DATA/active" = {
                     mountpoint = "/DATA";
                     mountOptions = [
                       "compress=zstd"
@@ -30,42 +29,6 @@ in
                   };
                   "/DATA/.snapshots" = {
                     mountpoint = "/DATA/.snapshots";
-                    mountOptions = [
-                      "compress=zstd"
-                      "nofail"
-                    ];
-                  };
-                };
-              };
-            };
-          };
-        };
-      };
-    };
-    disk = {
-      ${disk5} = {
-        type = "disk";
-        device = "${disk5}";
-        content = {
-          type = "gpt";
-          partitions = {
-            DATA = {
-              label = "Archive";
-              size = "100%";
-              content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ];
-                subvolumes = {
-                  "/mnt/Archive" = { };
-                  "/Archive/rootfs" = {
-                    mountpoint = "/Archive";
-                    mountOptions = [
-                      "compress=zstd"
-                      "nofail"
-                    ];
-                  };
-                  "/Archive/.snapshots" = {
-                    mountpoint = "/Archive/.snapshots";
                     mountOptions = [
                       "compress=zstd"
                       "nofail"
