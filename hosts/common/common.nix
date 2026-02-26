3c4344a3be1c19d65f6331cd356f3cc3ddf3a7ef
ajhyperbit <ajhyperbit@gmail.com>
2026-02-26 17:09:01 -0600

    add correct binary cache

--- hosts/common/common.nix
+++ hosts/common/common.nix
@@ -110,7 +110,7 @@ in
         "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
-        "https://hydra.lantian.pub/jobset/lantian/nix-cachyos-kernel"
+        "https://attic.xuyh0120.win/lantian" #"https://hydra.lantian.pub/jobset/lantian/nix-cachyos-kernel"
       ];
 
       trusted-public-keys = [
@@ -118,7 +118,6 @@ in
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       ];
     };
     #gc = {
