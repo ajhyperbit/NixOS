a4c77f613dd1f688c5a532d8d709beb4ded590ef
ajhyperbit <ajhyperbit@gmail.com>
2026-02-27 12:38:01 -0600

    missed a couple things

--- hosts/nixos/cachyos-kernel.nix
+++ hosts/nixos/cachyos-kernel.nix
@@ -1,8 +1,8 @@
-{ pkgs, nix-cachyos-kernel, ... }:
+{ config, lib, pkgs, nix-cachyos-kernel, ... }:
 {
   nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned ]; # Force usage of binary cache if possible.
 
-  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+  boot.kernelPackages = lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-latest;
 
   nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
   nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
