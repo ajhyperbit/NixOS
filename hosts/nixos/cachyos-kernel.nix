65f180e0d6af5476437e8087a7b9fd61d68533f5
ajhyperbit <ajhyperbit@gmail.com>
2026-02-27 15:37:35 -0600

    cachyos-kernel.nix: remove the unnecesary lib.mkForce

--- hosts/nixos/cachyos-kernel.nix
+++ hosts/nixos/cachyos-kernel.nix
@@ -2,7 +2,7 @@
 {
   nixpkgs.overlays = [ nix-cachyos-kernel.overlays.pinned ]; # Force usage of binary cache if possible.
 
-  boot.kernelPackages = lib.mkForce pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+  boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
 
   nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
   nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
