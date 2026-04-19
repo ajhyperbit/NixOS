53f237dad2c8b7d7fcee9e668c52b9480869d3c8
ajhyperbit <ajhyperbit@gmail.com>
2026-04-19 22:55:00 +0000

    remove initial password since no longer useful

--- hosts/common/init.nix
+++ hosts/common/init.nix
@@ -38,7 +38,7 @@
   };
 
   users.users."${username}" ={
-    initialHashedPassword = "$y$j9T$54o/3RK4qAaVQQIJhxrWQ1$9E3Fs.KWkEY03wrnoHvN7ZlQ4TwXzqv431lgkLGuYQ0";
+    #initialHashedPassword = "$y$j9T$54o/3RK4qAaVQQIJhxrWQ1$9E3Fs.KWkEY03wrnoHvN7ZlQ4TwXzqv431lgkLGuYQ0";
   };
 
   networking.networkmanager.enable = true;
