207edf2f3b4ceecb2ba5e808847865853b5dd595
ajhyperbit <ajhyperbit@gmail.com>
2026-04-19 16:00:56 -0500

    add an initial hashed password so I am not locked out on initial install

--- hosts/common/users.nix
+++ hosts/common/users.nix
@@ -25,8 +25,7 @@ in
       ];
 
       # define user packages here
-      packages = with pkgs; [
-      ];
+      initialHashedPassword = "$y$j9T$54o/3RK4qAaVQQIJhxrWQ1$9E3Fs.KWkEY03wrnoHvN7ZlQ4TwXzqv431lgkLGuYQ0";
     };
 
     defaultUserShell = pkgs.zsh;
