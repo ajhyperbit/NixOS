3b0abbc4dac62dfdcf074f970c7a98946d576da6
ajhyperbit <ajhyperbit@gmail.com>
2026-04-19 15:25:06 -0500

    even more major reduction

--- flake.nix
+++ flake.nix
@@ -277,10 +277,10 @@
           modules = [
             ./hosts/${otg-host}/config.nix
             #./hosts/${otg-host}/ai.nix
-            ./hosts/${host}/gpg-agent.nix
+            #./hosts/${host}/gpg-agent.nix
             #./hosts/${otg-host}/drives.nix
             ./hosts/${otg-host}/disko/single-drive-setup.nix
-            ./hosts/${host}/input.nix
+            #./hosts/${host}/input.nix
             ./hosts/${otg-host}/${otg-host}-hm.nix
             ./hosts/${host}/cachyos-kernel.nix
             ./hosts/${otg-host}/sys-ver.nix
