8b48e5c281709bd25b48883fba7c748968a96fb3
ajhyperbit <ajhyperbit@gmail.com>
2026-04-19 15:32:39 -0500

    flake.nix: add a minimal otg as well just incase

--- flake.nix
+++ flake.nix
@@ -257,7 +257,7 @@
             )
           ];
         };
-        
+
         "${otg-host}-reduced" = nixpkgs.lib.nixosSystem rec {
           system = "x86_64-linux";
           specialArgs = {
@@ -295,6 +295,48 @@
             ./hosts/common/temp-fixes.nix
             # ./hosts/common/overlays.nix
             ./hosts/common/virtualization.nix
+            home-manager.nixosModules.home-manager
+            nixos-hardware.nixosModules.common-pc-ssd
+            stylix.nixosModules.stylix
+            disko.nixosModules.disko
+            nix-index-database.nixosModules.nix-index
+
+            (
+              {
+                self,
+                ...
+              }:
+              {
+                environment.systemPackages =
+                  with self.inputs.nix-alien.packages.${pkgs.stdenv.hostPlatform.system}; [
+                    nix-alien
+                  ];
+              }
+            )
+          ];
+        };
+
+        "${otg-host}-minimal" = nixpkgs.lib.nixosSystem rec {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit system;
+            inherit inputs;
+            inherit username;
+            inherit host;
+            inherit otg-host;
+            inherit home;
+            inherit self;
+            inherit cursor_size;
+            inherit cursor_theme;
+            inherit pkgs-d49b5ff;
+            inherit pkgs-sliding;
+            inherit nix-cachyos-kernel;
+          };
+          modules = [
+            ./hosts/${otg-host}/config.nix
+            ./hosts/${otg-host}/disko/single-drive-setup.nix
+            ./hosts/${otg-host}/sys-ver.nix
+            ./hosts/common/users.nix
             home-manager.nixosModules.home-manager
             nixos-hardware.nixosModules.common-pc-ssd
             stylix.nixosModules.stylix
