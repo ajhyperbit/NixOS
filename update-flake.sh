97dc16ad10a04d061eb6446c0bc06a8e19bd7f2e
ajhyperbit <ajhyperbit@gmail.com>
2026-05-03 18:53:01 -0500

    add update comment for everything aside from hyprland

--- update-flake.sh
+++ update-flake.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 #Update flake.lock
 nix flake update
+#Update everything aside from hyprland
+#nix flake update nixpkgs nixpkgs-d49b5ff nixpkgs-sliding-commit flake-compact flake-parts nixpkgs-lib nix-index-database home-manager quickshell nixos-hardware stylix alejandra fw-fanctrl nix-alien disko rose-pine-hyprcursor nix4vscode nix-vscode-extensions nix-cachyos-kernel
 #Add flake.lock to staged changes
 git add flake.lock
 #Handle if we're connected via SSH
