6a1f2bd8f5a1cf49ffb267926b0f8575f1a775e9
ajhyperbit <ajhyperbit@gmail.com>
2026-05-03 23:18:43 -0500

    added comment

--- hosts/common/hyprland/temp-hyprland.nix
+++ hosts/common/hyprland/temp-hyprland.nix
@@ -8,6 +8,7 @@
 {
   programs = {
     hyprland = {
+      #Worked to get v0.53.3 installed properly
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland.overrideAttrs (old: {
         postPatch = (old.postPatch or "") + ''
           sed -i 's/find_package(glaze 6\.0\.0 QUIET)/find_package(glaze QUIET)/' hyprpm/CMakeLists.txt
