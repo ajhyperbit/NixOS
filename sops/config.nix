a379c4f4a89daef9263b45700f0107f42f7e4388
ajhyperbit <ajhyperbit@gmail.com>
2026-05-30 18:35:36 -0500

    swap to a pure json based noctalia config instead of maintaining nix attrs manually. Change sops key location, hopefully reboots will occur correctly now

--- sops/config.nix
+++ sops/config.nix
@@ -12,7 +12,8 @@
   sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
-    age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
+    # age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
+    age.keyFile = "/etc/sops/age/keys.txt";
   };
 
   environment.systemPackages = with pkgs; [
