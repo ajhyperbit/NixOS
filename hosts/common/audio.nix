fca7c073ec22b3f181d9f34066a0411edc95eba2
ajhyperbit <ajhyperbit@gmail.com>
2026-06-15 12:00:46 -0500

    made audio changes to hopefully make it better. Removed the ability to have HFP devices. Abstracted away some things into defaultMatchCriteria and bt_headset_props

--- hosts/common/audio.nix
+++ hosts/common/audio.nix
@@ -1,127 +1,118 @@
-{ pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    pwvucontrol # Pipewire Volume Control
-    easyeffects # Audio effects for PipeWire applications
-  ];
-  lib.mkMerge = {
-    services = {
-      pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        jack.enable = true;
-        extraConfig = {
-          pipewire."92-low-latency" = {
-            "context.properties" = {
-              "default.clock.rate" = 48000;
-              "default.clock.quantum" = 32;
-              "default.clock.min-quantum" = 32;
-              "default.clock.max-quantum" = 32;
-            };
-          };
-          pipewire-pulse."92-low-latency" = {
-            context.modules = [
-              {
-                name = "libpipewire-module-protocol-pulse";
-                args = {
-                  pulse.min.req = "32/48000";
-                  pulse.default.req = "32/48000";
-                  pulse.max.req = "32/48000";
-                  pulse.min.quantum = "32/48000";
-                  pulse.max.quantum = "32/48000";
-                };
-              }
-            ];
-            stream.properties = {
-              node.latency = "32/48000";
-              resample.quality = 1;
-            };
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  defaultMatchCriteria = {
+    "device.name" = "~bluez_card.*";
+    "device.vendor.id" = "usb:054c";
+  };
+  bt_headset_props = {
+    # Set quality to high quality instead of the default of auto
+    "bluez5.a2dp.ldac.quality" = "hq";
+    #"bluez5.auto-connect" = "[a2dp_sink]"
+    #"device.profile" = "a2dp-sink";
+  };
+in
+{
+  options.audio.disableHfpHsp = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "Disable HFP/HSP bluetooth profiles in WirePlumber. Set to false to re-enable voice/call profiles.";
+  };
+  
+  config = {
+    environment.systemPackages = with pkgs; [
+      pwvucontrol
+      easyeffects
+    ];
+
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+      extraConfig = {
+        pipewire."92-low-latency" = {
+          "context.properties" = {
+            "default.clock.rate" = 48000;
+            "default.clock.quantum" = 32;
+            "default.clock.min-quantum" = 32;
+            "default.clock.max-quantum" = 32;
           };
         };
-        wireplumber = {
-          configPackages = [
-            (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/11-bluetooth-policy.conf" ''
-              wireplumber.settings = {
-                bluetooth.autoswitch-to-headset-profile = false
-              }'')
+        pipewire-pulse."92-low-latency" = {
+          context.modules = [
+            {
+              name = "libpipewire-module-protocol-pulse";
+              args = {
+                pulse.min.req = "32/48000";
+                pulse.default.req = "32/48000";
+                pulse.max.req = "32/48000";
+                pulse.min.quantum = "32/48000";
+                pulse.max.quantum = "32/48000";
+              };
+            }
           ];
-          extraConfig = {
+          stream.properties = {
+            node.latency = "32/48000";
+            resample.quality = 1;
+          };
+        };
+      };
+      wireplumber = {
+        configPackages = [
+          (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/11-bluetooth-policy.conf" ''
+            wireplumber.settings = {
+              bluetooth.autoswitch-to-headset-profile = false
+            }'')
+        ];
+        extraConfig = lib.mkMerge [
+          (lib.mkIf config.audio.disableHfpHsp {
+            "disable-hfphsp" = {
+              "monitor.bluez.properties" = {
+                "bluez5.hfphsp-backend" = "none";
+              };
+            };
+          })
+          {
             "log-level-debug" = {
               "context.properties" = {
-                # Output Debug log messages as opposed to only the default level (Notice)
                 "log.level" = "D";
               };
             };
             "wh-1000xm3-ldac-hq" = {
               "monitor.bluez.rules" = [
                 {
-                  matches = [
-                    {
-                      # Match any bluetooth device with ids equal to that of a WH-1000XM3
-                      "device.name" = "~bluez_card.*";
-                      "device.product.id" = "0x0cd3";
-                      "device.vendor.id" = "usb:054c";
-                    }
-                  ];
-                  actions = {
-                    update-props = {
-                      # Set quality to high quality instead of the default of auto
-                      "bluez5.a2dp.ldac.quality" = "hq";
-                      #1"bluez5.auto-connect" = "[a2dp_sink]"
-                      #"device.profile" = "a2dp-sink";
-                    };
-                  };
+                  # Match any bluetooth device with ids equal to that of a WH-1000XM3
+                  matches = [ (defaultMatchCriteria // { "device.product.id" = "0x0cd3"; }) ];
+                  actions.update-props = bt_headset_props;
                 }
               ];
             };
             "wh-1000xm4-ldac-hq" = {
               "monitor.bluez.rules" = [
                 {
-                  matches = [
-                    {
-                      # Match any bluetooth device with ids equal to that of a WH-1000XM6
-                      "device.name" = "~bluez_card.*";
-                      "device.product.id" = "0x0d58";
-                      "device.vendor.id" = "usb:054c";
-                    }
-                  ];
-                  actions = {
-                    update-props = {
-                      # Set quality to high quality instead of the default of auto
-                      "bluez5.a2dp.ldac.quality" = "hq";
-                      #1"bluez5.auto-connect" = "[a2dp_sink]"
-                      #"device.profile" = "a2dp-sink";
-                    };
-                  };
+                  # Match any bluetooth device with ids equal to that of a WH-1000XM4
+                  matches = [ (defaultMatchCriteria // { "device.product.id" = "0x0d58"; }) ];
+                  actions.update-props = bt_headset_props;
                 }
               ];
             };
             "wh-1000xm6-ldac-hq" = {
               "monitor.bluez.rules" = [
                 {
-                  matches = [
-                    {
-                      # Match any bluetooth device with ids equal to that of a WH-1000XM6
-                      "device.name" = "~bluez_card.*";
-                      "device.product.id" = "0x0f8a";
-                      "device.vendor.id" = "usb:054c";
-                    }
-                  ];
-                  actions = {
-                    update-props = {
-                      # Set quality to high quality instead of the default of auto
-                      "bluez5.a2dp.ldac.quality" = "hq";
-                      #1"bluez5.auto-connect" = "[a2dp_sink]"
-                      #"device.profile" = "a2dp-sink";
-                    };
-                  };
+                  # Match any bluetooth device with ids equal to that of a WH-1000XM6
+                  matches = [ (defaultMatchCriteria // { "device.product.id" = "0x0f8a"; }) ];
+                  actions.update-props = bt_headset_props;
                 }
               ];
             };
-          };
-        };
+          }
+        ];
       };
     };
   };
