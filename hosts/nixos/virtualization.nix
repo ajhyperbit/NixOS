0b4cb0d6ed925d48d686fb233dfdec6e12890424
ajhyperbit <ajhyperbit@gmail.com>
2026-06-18 14:51:51 -0500

    final setup for 3050 with windows. Added udev rule to force the GPU into a lower power state when not used by a VM

--- hosts/nixos/virtualization.nix
+++ hosts/nixos/virtualization.nix
@@ -3,7 +3,7 @@
     "amd_iommu=on"
     "iommu=pt"
     # Blindfold the host from touching the RTX 3050
-    #"vfio-pci.ids=10de:2507,10de:228e"
+    "vfio-pci.ids=10de:2584,10de:2291"
   ];
 
   boot.initrd.kernelModules = [
@@ -13,6 +13,14 @@
     "vfio_iommu_type1"
     "kvm-amd"
   ];
+
+  services.udev.extraRules = ''
+    # Enable runtime power management for the RTX 3050 Video Controller
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{device}=="0x2584", ATTR{power/control}="auto"
+
+    # Enable runtime power management for the RTX 3050 Audio Controller
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{device}=="0x2291", ATTR{power/control}="auto"
+  '';
 
   # Create the Inter-VM Shared Memory (IVSHMEM) layer for Looking Glass
   systemd.tmpfiles.rules = [
