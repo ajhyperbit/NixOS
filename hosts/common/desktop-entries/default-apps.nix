531e9e8c44c9d29692d5b60416595744f421f2eb
ajhyperbit <ajhyperbit@gmail.com>
2026-06-24 08:17:00 -0500

    changed how default apps are handled and changed pdf app default from chrome to floorp

--- hosts/common/desktop-entries/default-apps.nix
+++ hosts/common/desktop-entries/default-apps.nix
@@ -1,18 +1,47 @@
-{ ... }: {
-  xdg = {
-    mime = {
-      defaultApplications = {
-        #Lists of mime types:
-        #Common types as defined by mozilla:
-        #https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types/Common_types
-        #Contains a database of many mime types:
-        #https://mimetype.io/all-types
-        #Offical IANA types:
-        #https://www.iana.org/assignments/media-types/media-types.xhtml
-        "application/pdf" = "google-chrome.desktop";
-        "inode/directory" = "thunar.desktop";
-        "text/x-patch" = "kate.desktop";
-      };
+{
+  lib,
+  config,
+  ...
+}:
+let
+  fileContent = builtins.readFile ./default-apps.list;
+
+  lines = lib.strings.splitString "\n" fileContent;
+
+  validLines = builtins.filter (
+    line: line != "" && !(lib.strings.hasPrefix "[" line) && (lib.strings.hasInfix "=" line)
+  ) lines;
+
+  parseLine =
+    line:
+    let
+      parts = lib.strings.splitString "=" line;
+      mimeType = builtins.head parts;
+
+      rawApp = lib.strings.concatStringsSep "=" (builtins.tail parts);
+      cleanApp = lib.strings.removeSuffix ";" rawApp;
+    in
+    {
+      name = mimeType;
+      value = cleanApp;
+    };
+
+  parsedMimeApps = builtins.listToAttrs (builtins.map parseLine validLines);
+in
+{
+  options = {
+    default-apps.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      example = true;
+    };
+  };
+
+  config = lib.mkIf config.default-apps.enable {
+    xdg.mime.defaultApplications = parsedMimeApps // {
+      "application/pdf" = "floorp.desktop";
+      "inode/directory" = "thunar.desktop";
+      "text/x-patch" = "kate.desktop";
     };
   };
 }
