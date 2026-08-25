15b9f8199e1360284058cea41e2c123965d130fd
ajhyperbit <ajhyperbit@gmail.com>
2026-08-25 17:24:48 -0500

    removed sqlite stuff

--- hosts/common/packages/pkg-cats/system-tools.nix
+++ hosts/common/packages/pkg-cats/system-tools.nix
@@ -52,7 +52,6 @@
         gomtree # File system tree validation against recorded manifests
         compsize # Find compression type/ratio in a Btrfs filesystem
         duperemove # Deduplication of files
-        sqlitebrowser # DB Browser for SQLite
 
         # ── Networking & Remote Access ────────────────────────────────────────────
         tailscale # WireGuard-based mesh VPN
